6f659b2b161f2520ed34016f0a6a980a6afe7ed7
Chiasmera <98319072+Chiasmera@users.noreply.github.com>
2023-12-07 11:03:11 +0100

    Passenger Booking done ASP

--- WebAPP/Controllers/FerryController.cs
+++ WebAPP/Controllers/FerryController.cs
@@ -1,6 +1,7 @@
 ﻿using Business_Logic;
 using Data_Transfer_Objects.Model;
 using Microsoft.AspNetCore.Mvc;
+using WebAPP.Models;
 
 namespace WebAPP.Controllers
 {
@@ -11,19 +12,22 @@ namespace WebAPP.Controllers
             return View();
         }
 
+        [HttpPost]
         public IActionResult OnActionSubmit(IFormCollection form)
         {
             string action = form["ferryAction"];
             int ferryID = 0;
-            bool idGood = Int32.TryParse( form["ferryList"], out ferryID);
-            if ( action == null || !idGood)
+            bool idGood = Int32.TryParse(form["ferryList"], out ferryID);
+            if (action == null || !idGood)
             {
                 ViewBag.error = "There was an error with the selected action. Try again";
                 FerryBL ferryBL = new FerryBL();
                 HashSet<Ferry> ferries = ferryBL.GetAll();
                 return RedirectToAction("Index", "Home");
 
-            } else {
+            }
+            else
+            {
                 if (action.Equals("Book Car"))
                 {
                     Car car = new Car();
@@ -31,8 +35,10 @@ namespace WebAPP.Controllers
                 }
                 else if (action.Equals("Book Passenger"))
                 {
-                    Passenger passenger = new Passenger();
-                    return View("BookPassenger", passenger);
+
+                    PassengerBooking passengerBooking = new PassengerBooking();
+                    passengerBooking.ferryID = ferryID;
+                    return View("BookPassenger", passengerBooking);
                 }
                 else
                 {
@@ -43,5 +49,54 @@ namespace WebAPP.Controllers
                 }
             }
         }
+
+        [HttpPost]
+        public IActionResult BookNewPassenger(PassengerBooking booking)
+        {
+            if (ModelState.IsValid
+                && booking.passengerName != null
+                && booking.passengerGender != null
+                && booking.passengerName.Trim().Length > 1
+                && booking.passengerGender.Trim().Length > 1
+                && booking.ferryID != 0
+                )
+            {
+                    Passenger passenger = new Passenger(0, booking.passengerName, booking.passengerGender);
+                    PassengerBL passengerBL = new PassengerBL();
+                    Passenger added = passengerBL.Add(passenger);
+                    FerryBL ferryBL = new FerryBL();
+                    Passenger reciept = ferryBL.AddPassenger(booking.ferryID, added.Id);
+                    return View("PassengerReciept", reciept);
+            } else
+            {
+                ViewBag.error = "There was an error with the selected action. Try again";
+                FerryBL ferryBL = new FerryBL();
+                HashSet<Ferry> ferries = ferryBL.GetAll();
+                return RedirectToAction("Index", "Home");
+            }
+        }
+    
+
+        [HttpPost]
+        public IActionResult BookPassengerById(PassengerBooking booking)
+        {
+        if (
+            booking.passengerID > 0
+            && booking.ferryID != 0
+            )
+        {
+                FerryBL ferryBl = new FerryBL();
+                Passenger reciept = ferryBl.AddPassenger(booking.ferryID, booking.passengerID);
+                return View("PassengerReciept", reciept);
+        }
+        else
+        {
+            ViewBag.error = "There was an error with the selected action. Try again";
+            FerryBL ferryBL = new FerryBL();
+            HashSet<Ferry> ferries = ferryBL.GetAll();
+            return RedirectToAction("Index", "Home");
+        }
+
+    }
     }
 }
---
+++ WebAPP/Models/PassengerBooking.cs
@@ -0,0 +1,24 @@
+﻿using Data_Transfer_Objects.Model;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebAPP.Models
+{
+    public class PassengerBooking
+    {
+
+        public int ferryID { get; set; }
+
+        [Display(Name = "ID")]
+        public int passengerID { get; set; }
+
+        [Display(Name ="Name")]
+        //[Required(ErrorMessage = "You forgot to enter your name")]
+        //[StringLength(120, MinimumLength = 2, ErrorMessage = "Name must be at least 2 characters")]
+        public string passengerName { get; set; }
+
+        [Display(Name = "Gender")]
+        //[Required(ErrorMessage = "You forgot to enter your name")]
+        //[StringLength(120, MinimumLength = 2, ErrorMessage = "Gender must be at least 2 characters")]
+        public string passengerGender { get; set; }
+    }
+}
